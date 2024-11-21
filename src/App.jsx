69aed88eb9e0614e29dfe954081fa7659ff75054
Aakash <chaudharyaakash234@gmail.com>
2024-11-21 21:34:22 +0545

    Todo App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Input from './component/Input/Input'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl tracking-tighter font-sans underline p-5'>Hi, user 👋</h1>
+      <h1 className='text-3xl tracking-tighter underline font-sans p-5'>Manage your Todos</h1>
+      <Input />
+    </>
+  )
+}
+
+export default App
